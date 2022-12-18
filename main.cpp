1aa69d26c7efd551ee823ae050e43c5739ab975f
kkaneko <45529381+kkaneko-42@users.noreply.github.com>
2022-12-18 22:50:32 +0900

    chapter 'ImageView' completed

--- main.cpp
+++ main.cpp
@@ -94,6 +94,8 @@ private:
 	VkFormat _swapChainImageFormat;
 	VkExtent2D _swapChainExtent;
 
+	std::vector<VkImageView> _swapChainImageViews;
+
 	struct QueueFamilyIndices {
 		std::optional<uint32_t> graphicsFamily;
 		std::optional<uint32_t> presentFamily;
@@ -141,6 +143,37 @@ private:
 		pickPhysicalDevice();
 		createLogicalDevice();
 		createSwapChain(); // 論理デバイス作成後に呼ぶ
+		createImageViews();
+	}
+
+	void createImageViews() {
+		_swapChainImageViews.resize(_swapChainImages.size());
+		for (std::size_t i = 0; i < _swapChainImages.size(); ++i) {
+			VkImageViewCreateInfo createInfo{};
+			createInfo.sType = VK_STRUCTURE_TYPE_IMAGE_VIEW_CREATE_INFO;
+			createInfo.image = _swapChainImages[i];
+
+			// 画像データをどのように解釈するか(2Dテクスチャ、3Dテクスチャ等)
+			createInfo.viewType = VK_IMAGE_VIEW_TYPE_2D;
+			createInfo.format = _swapChainImageFormat;
+
+			// FROM tutorial: swizzle...メソッドの機能を動的に変更する
+			createInfo.components.r = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.g = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.b = VK_COMPONENT_SWIZZLE_IDENTITY;
+			createInfo.components.a = VK_COMPONENT_SWIZZLE_IDENTITY;
+
+			createInfo.subresourceRange.aspectMask = VK_IMAGE_ASPECT_COLOR_BIT;
+			createInfo.subresourceRange.baseMipLevel = 0;
+			createInfo.subresourceRange.levelCount = 1;
+			createInfo.subresourceRange.baseArrayLayer = 0;
+			createInfo.subresourceRange.layerCount = 1;
+
+			VkResult result = vkCreateImageView(_device, &createInfo, nullptr, &_swapChainImageViews[i]);
+			if (result != VK_SUCCESS) {
+				throw std::runtime_error("failed to create image views!");
+			}
+		}
 	}
 
 	// NOTE: 論理デバイス作成後に呼び出されることを期待
@@ -519,6 +552,10 @@ private:
 	}
 
 	void cleanup() {
+		for (auto imageView : _swapChainImageViews) {
+			vkDestroyImageView(_device, imageView, nullptr);
+		}
+
 		vkDestroySwapchainKHR(_device, _swapChain, nullptr);
 
 		vkDestroyDevice(_device, nullptr);
